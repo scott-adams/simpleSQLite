43ebf4190c0d6c46ee584f24c06eea380ceb9a02
Scott Adams <scott.adams@deakin.edu.au>
2016-04-30 00:08:02 +1000

    Added an add paper function to allow for simple adding of papers to the database

--- simpleSQLite.go
+++ simpleSQLite.go
@@ -6,13 +6,25 @@ import (
 	"html/template"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
+	"strconv"
 	"log"
 )
 
 type PaperStruct struct{
-	authors string
-	year int
-	title string
+	ID int
+	Authors string
+	Year int
+	Title string
+	Title2 string
+	Tag1 string
+	Tag2 string
+	Tag3 string
+	Tag4 string
+	Notes string
+}
+
+type Page struct{
+	Title string
 }
 
 var db *sql.DB //global database connection
@@ -24,12 +36,11 @@ func main() {
 	defer db.Close()
 
 	createTable()
-	c := PaperStruct{authors: "john smith", year: 1996, title: "foo"}
-	insertData(c)
-	//x := retrieveData()
-	//fmt.Printf("%+v\n", x)
 
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", viewFunc)
+	http.HandleFunc("/add/",addFunc)
+	http.HandleFunc("/save/",saveFunc)
+	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources"))))
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -39,11 +50,37 @@ func checkErr(err error) {
         }
 }
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func viewFunc(w http.ResponseWriter, r *http.Request) {
 	x := retrieveData()
-	t,err := template.ParseFiles("template.html")
+	t,err := template.ParseFiles("templates/view_template.html")
 	checkErr(err)
-	t.Execute(w, x)
+	err = t.Execute(w,x)
+	checkErr(err)
+}
+
+func addFunc(w http.ResponseWriter, r *http.Request) {
+	x:= Page{Title: "Add Record"}
+	t,err := template.ParseFiles("templates/add_template.html")
+	checkErr(err)
+	err = t.Execute(w,x)
+	checkErr(err)
+}
+
+func saveFunc(w http.ResponseWriter, r *http.Request) {
+	yearInt, err := strconv.Atoi(r.FormValue("Year"))
+	checkErr(err)
+	c := PaperStruct{
+		Authors:r.FormValue("Authors"),
+		Year:yearInt,
+		Title:r.FormValue("Title"),
+		Title2:r.FormValue("Title2"),
+		Tag1:r.FormValue("Tag1"),
+		Tag2:r.FormValue("Tag2"),
+		Tag3:r.FormValue("Tag3"),
+		Tag4:r.FormValue("Tag4"),
+		Notes:r.FormValue("Notes")}
+	insertData(c)
+	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 //create the table if it doesn't exist
@@ -52,9 +89,15 @@ func createTable(){
 	sqlStmt := `
 	create table if not exists papers(
 		id integer not null primary key, 
-		title text, 
+		authors text, 
 		year integer, 
-		authors text);`
+		title text,
+		title2 text,
+		tag1 text,
+		tag2 text,
+		tag3 text,
+		tag4 text,
+		notes text);`
 	_, err := db.Exec(sqlStmt)
 	if err != nil {
 		log.Printf("%q: %s\n", err, sqlStmt)
@@ -63,20 +106,20 @@ func createTable(){
 }
 
 func insertData(c PaperStruct){
-	_,err := db.Exec(fmt.Sprintf("insert into papers(title,year,authors) values('%s',%d,'%s')",c.title,c.year,c.authors))
+	_,err := db.Exec(fmt.Sprintf("insert into papers(authors,year,title,title2,tag1,tag2,tag3,tag4,notes) values('%s',%d,'%s','%s','%s','%s','%s','%s','%s')",c.Authors,c.Year,c.Title,c.Title2,c.Tag1,c.Tag2,c.Tag3,c.Tag4,c.Notes))
 	checkErr(err)
 }
 
 func retrieveData()[]PaperStruct{
 	//make a slice of paper structs of length 0
 	results := make([]PaperStruct,0)
-	rows, err := db.Query("select title,year,authors from papers")
+	rows, err := db.Query("select * from papers")
 	checkErr(err)
 
 	defer rows.Close()
 	var table_data PaperStruct
 	for rows.Next() {
-		err = rows.Scan(&table_data.title,&table_data.year,&table_data.authors)
+		err = rows.Scan(&table_data.ID,&table_data.Authors,&table_data.Year,&table_data.Title,&table_data.Title2,&table_data.Tag1,&table_data.Tag2,&table_data.Tag3,&table_data.Tag4,&table_data.Notes)
 		checkErr(err)
 		results = append(results, table_data)
 	}
