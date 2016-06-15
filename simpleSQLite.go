f761bee866535c2b561aebab9edb628510b2e126
scott-adams <scottya@10-108-14-66.wifi-g.deakin.edu.au>
2016-06-15 14:37:40 +1000

    Cleaned up the code according to gofmt

--- simpleSQLite.go
+++ simpleSQLite.go
@@ -2,28 +2,31 @@ package main
 
 import (
 	"database/sql"
-	"net/http"
 	"html/template"
+	"net/http"
 	//"fmt"
-	_ "github.com/mattn/go-sqlite3"
-	"strconv"
 	"log"
+	"strconv"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type PaperStruct struct{
-	ID int
+//A PaperStruct is a struct that describes the structure of a single research paper
+type PaperStruct struct {
+	ID      int
 	Authors string
-	Year int
-	Title string
-	Title2 string
-	Tag1 string
-	Tag2 string
-	Tag3 string
-	Tag4 string
-	Notes string
+	Year    int
+	Title   string
+	Title2  string
+	Tag1    string
+	Tag2    string
+	Tag3    string
+	Tag4    string
+	Notes   string
 }
 
-type Page struct{
+//A Page is a struct that describes a single HTML Page
+type Page struct {
 	Title string
 }
 
@@ -38,34 +41,34 @@ func main() {
 	createTable()
 
 	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/view/",viewHandler)
-	http.HandleFunc("/add/",addHandler)
-	http.HandleFunc("/save/",saveHandler)
-	http.HandleFunc("/edit/",editHandler)
-	http.HandleFunc("/update/",updateHandler)
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/add/", addHandler)
+	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/update/", updateHandler)
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources"))))
 	http.ListenAndServe(":8080", nil)
 }
 
 func checkErr(err error) {
-        if err != nil {
-            log.Fatal(err)
-        }
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	x := retrieveMultiLineData()
-	t,err := template.ParseFiles("templates/view_template.html")
+	t, err := template.ParseFiles("templates/view_template.html")
 	checkErr(err)
-	err = t.Execute(w,x)
+	err = t.Execute(w, x)
 	checkErr(err)
 }
 
 func addHandler(w http.ResponseWriter, r *http.Request) {
-	x:= Page{Title: "Add Record"}
-	t,err := template.ParseFiles("templates/add_template.html")
+	x := Page{Title: "Add Record"}
+	t, err := template.ParseFiles("templates/add_template.html")
 	checkErr(err)
-	err = t.Execute(w,x)
+	err = t.Execute(w, x)
 	checkErr(err)
 }
 
@@ -77,27 +80,27 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	yearInt, err := strconv.Atoi(r.FormValue("Year"))
 	checkErr(err)
 	c := PaperStruct{
-		Authors:r.FormValue("Authors"),
-		Year:yearInt,
-		Title:r.FormValue("Title"),
-		Title2:r.FormValue("Title2"),
-		Tag1:r.FormValue("Tag1"),
-		Tag2:r.FormValue("Tag2"),
-		Tag3:r.FormValue("Tag3"),
-		Tag4:r.FormValue("Tag4"),
-		Notes:r.FormValue("Notes")}
+		Authors: r.FormValue("Authors"),
+		Year:    yearInt,
+		Title:   r.FormValue("Title"),
+		Title2:  r.FormValue("Title2"),
+		Tag1:    r.FormValue("Tag1"),
+		Tag2:    r.FormValue("Tag2"),
+		Tag3:    r.FormValue("Tag3"),
+		Tag4:    r.FormValue("Tag4"),
+		Notes:   r.FormValue("Notes")}
 	insertData(c)
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	recordID,err := strconv.Atoi(r.URL.Path[6:]) //strip "/edit/" from the URL string and convert to int
+	recordID, err := strconv.Atoi(r.URL.Path[6:]) //strip "/edit/" from the URL string and convert to int
 	checkErr(err)
 	record := retrieveRecord(recordID)
-	t,err := template.ParseFiles("templates/edit_template.html")
-        checkErr(err)
-        err = t.Execute(w,record)
-        checkErr(err)
+	t, err := template.ParseFiles("templates/edit_template.html")
+	checkErr(err)
+	err = t.Execute(w, record)
+	checkErr(err)
 }
 
 func updateHandler(w http.ResponseWriter, r *http.Request) {
@@ -106,28 +109,28 @@ func updateHandler(w http.ResponseWriter, r *http.Request) {
 	idInt, err := strconv.Atoi(r.FormValue("ID"))
 	checkErr(err)
 	c := PaperStruct{
-	        Authors:r.FormValue("Authors"),
-                Year:yearInt,
-                Title:r.FormValue("Title"),
-                Title2:r.FormValue("Title2"),
-                Tag1:r.FormValue("Tag1"),
-                Tag2:r.FormValue("Tag2"),
-                Tag3:r.FormValue("Tag3"),
-                Tag4:r.FormValue("Tag4"),
-                Notes:r.FormValue("Notes")}
-	_,err = db.Exec("update papers set authors=?,year=?,title=?,title2=?,tag1=?,tag2=?,tag3=?,tag4=?,notes=? where id = ?",c.Authors,c.Year,c.Title,c.Title2,c.Tag1,c.Tag2,c.Tag3,c.Tag4,c.Notes,idInt)
+		Authors: r.FormValue("Authors"),
+		Year:    yearInt,
+		Title:   r.FormValue("Title"),
+		Title2:  r.FormValue("Title2"),
+		Tag1:    r.FormValue("Tag1"),
+		Tag2:    r.FormValue("Tag2"),
+		Tag3:    r.FormValue("Tag3"),
+		Tag4:    r.FormValue("Tag4"),
+		Notes:   r.FormValue("Notes")}
+	_, err = db.Exec("update papers set authors=?,year=?,title=?,title2=?,tag1=?,tag2=?,tag3=?,tag4=?,notes=? where id = ?", c.Authors, c.Year, c.Title, c.Title2, c.Tag1, c.Tag2, c.Tag3, c.Tag4, c.Notes, idInt)
 	checkErr(err)
-        http.Redirect(w, r, "/", http.StatusFound)
+	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 //create the table if it doesn't exist
-func createTable(){
+func createTable() {
 	//create the table if it doesn't exist
 	sqlStmt := `
 	create table if not exists papers(
-		id integer not null primary key, 
-		authors text, 
-		year integer, 
+		id integer not null primary key,
+		authors text,
+		year integer,
 		title text,
 		title2 text,
 		tag1 text,
@@ -142,35 +145,35 @@ func createTable(){
 	}
 }
 
-func insertData(c PaperStruct){
-	_,err := db.Exec("insert into papers(authors,year,title,title2,tag1,tag2,tag3,tag4,notes) values(?,?,?,?,?,?,?,?,?)",c.Authors,c.Year,c.Title,c.Title2,c.Tag1,c.Tag2,c.Tag3,c.Tag4,c.Notes)
+func insertData(c PaperStruct) {
+	_, err := db.Exec("insert into papers(authors,year,title,title2,tag1,tag2,tag3,tag4,notes) values(?,?,?,?,?,?,?,?,?)", c.Authors, c.Year, c.Title, c.Title2, c.Tag1, c.Tag2, c.Tag3, c.Tag4, c.Notes)
 	checkErr(err)
 }
 
-func retrieveRecord(id int)PaperStruct{
-	row,err := db.Query("select * from papers where ID = ?",id)
+func retrieveRecord(id int) PaperStruct {
+	row, err := db.Query("select * from papers where ID = ?", id)
 	checkErr(err)
 	defer row.Close()
-	var table_data PaperStruct
-	for row.Next(){
-		err = row.Scan(&table_data.ID,&table_data.Authors,&table_data.Year,&table_data.Title,&table_data.Title2,&table_data.Tag1,&table_data.Tag2,&table_data.Tag3,&table_data.Tag4,&table_data.Notes)
-	checkErr(err)
-}
-	return table_data
+	var tableData PaperStruct
+	for row.Next() {
+		err = row.Scan(&tableData.ID, &tableData.Authors, &tableData.Year, &tableData.Title, &tableData.Title2, &tableData.Tag1, &tableData.Tag2, &tableData.Tag3, &tableData.Tag4, &tableData.Notes)
+		checkErr(err)
+	}
+	return tableData
 }
 
-func retrieveMultiLineData()[]PaperStruct{
+func retrieveMultiLineData() []PaperStruct {
 	//make a slice of paper structs of length 0
-	results := make([]PaperStruct,0)
+	var results []PaperStruct
 	rows, err := db.Query("select * from papers")
 	checkErr(err)
 
 	defer rows.Close()
-	var table_data PaperStruct
+	var tableData PaperStruct
 	for rows.Next() {
-		err = rows.Scan(&table_data.ID,&table_data.Authors,&table_data.Year,&table_data.Title,&table_data.Title2,&table_data.Tag1,&table_data.Tag2,&table_data.Tag3,&table_data.Tag4,&table_data.Notes)
+		err = rows.Scan(&tableData.ID, &tableData.Authors, &tableData.Year, &tableData.Title, &tableData.Title2, &tableData.Tag1, &tableData.Tag2, &tableData.Tag3, &tableData.Tag4, &tableData.Notes)
 		checkErr(err)
-		results = append(results, table_data)
+		results = append(results, tableData)
 	}
 	err = rows.Err()
 	checkErr(err)
